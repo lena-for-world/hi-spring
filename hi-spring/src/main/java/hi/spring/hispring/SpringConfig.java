fd394c6120d25c88f6a2dd4b6f57c5237abc6532
margaerhy <kimrlyunah@gmail.com>
2021-07-06 22:22:05 +0900

    Add JDBC

--- hi-spring/src/main/java/hi/spring/hispring/SpringConfig.java
+++ hi-spring/src/main/java/hi/spring/hispring/SpringConfig.java
@@ -1,13 +1,24 @@
 package hi.spring.hispring;
 
+import hi.spring.hispring.repository.JdbcMemberRepository;
 import hi.spring.hispring.repository.MemberRepository;
 import hi.spring.hispring.repository.MemoryMemberRepository;
 import hi.spring.hispring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     // Component 수동등록
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
