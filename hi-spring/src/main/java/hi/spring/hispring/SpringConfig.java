9acb5ee06fe357de2c3303e4be754457892dfc77
margaerhy <kimrlyunah@gmail.com>
2021-07-07 23:59:30 +0900

    Add JDBCTemplate, JPA

--- hi-spring/src/main/java/hi/spring/hispring/SpringConfig.java
+++ hi-spring/src/main/java/hi/spring/hispring/SpringConfig.java
@@ -1,22 +1,23 @@
 package hi.spring.hispring;
 
-import hi.spring.hispring.repository.JdbcMemberRepository;
-import hi.spring.hispring.repository.MemberRepository;
-import hi.spring.hispring.repository.MemoryMemberRepository;
+import hi.spring.hispring.repository.*;
 import hi.spring.hispring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
     private final DataSource dataSource;
+    private final EntityManager em;
 
-    public SpringConfig(DataSource dataSource) {
+    public SpringConfig(DataSource dataSource, EntityManager em) {
         this.dataSource = dataSource;
+        this.em = em;
     }
 
     // Component 수동등록
@@ -27,6 +28,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new JdbcMemberRepository(dataSource);
+        // return new MemoryMemberRepository();
+        // return new JDbcMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
--- hi-spring/src/main/java/hi/spring/hispring/service/MemberService.java
+++ hi-spring/src/main/java/hi/spring/hispring/service/MemberService.java
@@ -5,10 +5,11 @@ import hi.spring.hispring.repository.MemberRepository;
 import hi.spring.hispring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
